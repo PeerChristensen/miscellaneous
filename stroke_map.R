7b58c9b4e6e95101406fe6a4c7e9e5b4872935b7
PeerChristensen <hr.pchristensen@gmail.com>
2019-06-10 09:45:38 +0200

    added more details

--- stroke_map.R
+++ stroke_map.R
@@ -1,24 +1,27 @@
+### mapping google searches for 'stroke' by region in Malaysia
 
 library(GADMTools)
 library(sf)
 library(gtrendsR)
 library(sp)
-
-my <- getData("GADM",country="MYS",level=1) # admin level 1 (region)
+library(raster)
+library(tmap)
 
 stroke <- gtrends(c("stroke"),
                   gprop = "web",
-                  time  = "2004-01-01 2019-03-31",
+                  #time  = "2004-01-01 2019-03-31",
                   geo   = c("MY"))
 
 stroke <- stroke$interest_by_region
 
-myMap <- getData("GADM",country="MYS",level=1)
+myMap <- getData("GADM",country="MYS",level=1,mask=TRUE)
+
+myMap@data$NAME_1 <- recode(myMap@data$NAME_1,"Trengganu"="Terengganu")
 
 strokeMerged  <- sp::merge(myMap,stroke,by.x = "NAME_1",by.y= "location")
 
 tmap_mode("view")  # for an interactive leaflet map
-tmap_mode("plot") # for a static map
+#tmap_mode("plot") # for a static map
 
 tm_shape(strokeMerged) +
   tm_polygons("hits") +
@@ -27,13 +30,13 @@ tm_shape(strokeMerged) +
   tm_text("NAME_1", auto.placement = TRUE,size = .7) +
   tm_view(set.zoom.limits = c(5,9))
 
+##########
 
+dk <- getData("GADM",country="DK",level=1)
 
-dk <- getData("GADM",country="DK",level=2)
-
-nm <- gtrends(c("Mallorca"),
+nm <- gtrends(c("mallorca"),
                   gprop = "web",
-                  time  = "2010-01-01 2019-03-31",
+                  #time  = "all",
                   geo   = c("DK"))
 
 nmReg <- nm$interest_by_region
@@ -44,11 +47,11 @@ nmReg$location <- recode(nmReg$location,"Region Zealand"="Sjælland",
                          "North Denmark Region" = "Nordjylland",
                          "Region Syddanmark" = "Syddanmark")
   
-nmMerged  <- sp::merge(dk,nmReg,by.x = "NAME_1",by.y= "location")
+nmMerged  <- sp::merge(za,nmReg,by.x = "NAME_1",by.y= "location")
 
 
-#tmap_mode("view")  # for an interactive leaflet map
-tmap_mode("plot") # for a static map
+tmap_mode("view")  # for an interactive leaflet map
+#tmap_mode("plot") # for a static map
 
 tm_shape(nmMerged) +
   tm_polygons("hits") +
