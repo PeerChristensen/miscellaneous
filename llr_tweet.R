8948cb1c65958b6a9e66cabdbd55c480e771550c
PeerChristensen <hr.pchristensen@gmail.com>
2019-09-01 09:05:26 +0200

    add scico

--- llr_tweet.R
+++ llr_tweet.R
@@ -4,20 +4,21 @@ library(tidyverse)
 library(tidytext)
 library(tm)
 library(wordcloud)
+library(scico)
 
 #llr <- search_users(q = "larsloekke", n = 1)
 
 #tweets_data(llr)
 
 # df <- search_tweets(
-#   "to:larsloekke", 
+#   "to:larsloekke",
 #   n = 1000,
 #   retryonratelimit = TRUE)
 # 
 # df <- df %>%
 #   filter(reply_to_status_id=="1167729739114500097")
 # 
-# df[sapply(df, is.list)] <- lapply(df[sapply(df, is.list)], 
+# df[sapply(df, is.list)] <- lapply(df[sapply(df, is.list)],
 #                                     as.character)
 # write_csv(df,"llr_replies.csv")
 
@@ -32,5 +33,5 @@ d <- df %>%
   count(word) %>%
   arrange(desc(n))
 
-wordcloud(d$word,d$n,min.freq = 10,colors = scico(n=30,palette = 'lajolla')[10:30] )  
+wordcloud(d$word,d$n,min.freq = 15,colors = scico(n=30,palette = 'lajolla')[10:30] )  
 
