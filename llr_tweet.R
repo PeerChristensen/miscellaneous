8b5a6a14de29c2eef3943e7693d6258940627fc4
PeerChristensen <hr.pchristensen@gmail.com>
2019-09-01 02:16:16 +0200

    llr wordcloud

---
+++ llr_tweet.R
@@ -0,0 +1,36 @@
+
+library(rtweet)
+library(tidyverse)
+library(tidytext)
+library(tm)
+library(wordcloud)
+
+#llr <- search_users(q = "larsloekke", n = 1)
+
+#tweets_data(llr)
+
+# df <- search_tweets(
+#   "to:larsloekke", 
+#   n = 1000,
+#   retryonratelimit = TRUE)
+# 
+# df <- df %>%
+#   filter(reply_to_status_id=="1167729739114500097")
+# 
+# df[sapply(df, is.list)] <- lapply(df[sapply(df, is.list)], 
+#                                     as.character)
+# write_csv(df,"llr_replies.csv")
+
+
+df <- read_csv("llr_replies.csv")
+
+d <- df %>%
+  select(screen_name,text) %>%
+  unnest_tokens(word,text) %>%
+  mutate(word = removeWords(word,c("larsloekke","lars","løkke","rasmussen","dit","så",tm::stopwords("da")))) %>%
+  filter(word !="") %>%
+  count(word) %>%
+  arrange(desc(n))
+
+wordcloud(d$word,d$n,min.freq = 10,colors = scico(n=30,palette = 'lajolla')[10:30] )  
+
