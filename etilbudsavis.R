7609e2e5b0c013e82ae48b1b3f784c524e292f80
PeerChristensen <hr.pchristensen@gmail.com>
2020-03-29 11:41:01 +0200

    upd

--- etilbudsavis.R
+++ etilbudsavis.R
@@ -13,11 +13,12 @@ query = glue::glue(base_url, query = "aperol",
 
 base_url <- "https://api.etilbudsavis.dk/v2/offers/search?query="
 
-search_item <- "kahlua"
+search_item <- "vodka"
 query = glue::glue("{base_url}{search_item}&r_lat=55.6656223&r_lng=12.6009489&r_radius=2000")
 
 data  =  jsonlite::fromJSON(query)
 
+heading <- data$heading
 store <- data$branding$name
 price <- data$pricing$price
 #quantity_unit <- data$quantity$unit$symbol # only works sometimes
@@ -27,7 +28,7 @@ from_day <- lubridate::wday(as.Date(data$run_from),label=T,abbr=F)
 until_date <- as.Date(data$run_till)
 until_day <- lubridate::wday(as.Date(data$run_till),label=T,abbr=F)
 
-df <- tibble(store,price,quantity_min,from_date,from_day,until_date,until_day)
+df <- tibble(heading,store,price,quantity_min,from_date,from_day,until_date,until_day)
 df
 
 d <- df %>% filter(price<119)
